f4dde84e3d88edda8358ce0f1b8b0442475d854d
jaydy <jaydy@qb.loni.org>
2015-02-18 16:28:46 -0600

    set up unittest 	new file:   load_test.C 	new file:   makefile

---
+++ src/load_test.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "size.h"
+#include "dock.h"
+#include "load.h"
+
+#include "gtest/gtest.h"
+
+TEST(Load_Ligand, 1a07C1_sdf){
+  Ligand0 *lig0 = new Ligand0[MAXEN2];
+  printf("testing ligand loading ...\n");
+  
+  delete[]lig0;
+}
